cfaf9cc0975842a1b4e66d48100791d7d07364ac
Dariia Lytvyn <ldasha60@gmail.com>
2020-12-24 17:51:49 +0700

    Add Task1-6 solution. Day 1

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int i = 10;
+        while (i>0){
+            i--;
+            System.out.print("JAVA ");
+        }
     }
 }
