8e70ba283d26f488b9a5a324ee16f4e1fcb98a97
Dariia Lytvyn <ldasha60@gmail.com>
2020-12-25 13:00:11 +0700

    Add day2 solutions + fix to day1

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -3,7 +3,7 @@ package day1;
 public class Task1 {
     public static void main(String[] args) {
         int i = 10;
-        while (i>0){
+        while (i > 0) {
             i--;
             System.out.print("JAVA ");
         }
--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,23 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        scanner.close();
+
+        if (k >= 1 && k <= 4) {
+            System.out.println("Малоэтажный дом");
+        } else if (k >= 5 && k <= 8) {
+            System.out.println("Среднеэтажный дом");
+        } else if (k >= 9) {
+            System.out.println("Многоэтажный дом");
+        } else if (k < 0) {
+            System.out.println("Ошибка ввода");
+        }
+
 
     }
 }
