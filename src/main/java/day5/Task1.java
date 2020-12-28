be11dcc6588dff164bee7308af1e993575477e7c
Dariia Lytvyn <ldasha60@gmail.com>
2020-12-28 23:23:00 +0700

    Add Day5 Solutions

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -3,5 +3,13 @@ package day5;
 public class Task1 {
     public static void main(String[] args) {
 
+        Car car1 = new Car();
+        car1.setYear(2014);
+        car1.setModel("Mazda");
+        car1.setColor("Red");
+
+        System.out.println(car1.getModel());
+        System.out.println(car1.getYear());
+        System.out.println(car1.getColor());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,10 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike1 = new Motorbike(2006,"Yamaha", "White");
+        System.out.println(motorbike1.getModel());
+        System.out.println(motorbike1.getYear());
+        System.out.println(motorbike1.getColor());
 
     }
 }
