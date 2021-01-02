ee487f8b72673c56784700491c821f29899f4bdf
Dariia Lytvyn <ldasha60@gmail.com>
2021-01-02 12:51:13 +0700

    Add Day7 Solution

---
+++ src/main/java/day7/Player.java
@@ -0,0 +1,47 @@
+package day7;
+
+public class Player {
+    private int stamina;
+    private final static int MAX_STAMINA = 100;
+    private final static int MIN_STAMINA = 0;
+    private static int countPlayers = 0;
+    private int freePlace = 0;
+
+    public Player(int stamina) {
+        this.stamina = stamina;
+
+        if (countPlayers < 6) {
+            countPlayers++;
+        }
+
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public static int getCountPlayers() {
+        return countPlayers;
+    }
+
+    public void run() {
+        if (stamina == 0) {
+            return;
+        }
+        stamina--;
+        if (stamina == 0) {
+            countPlayers--;
+        }
+
+    }
+
+    public void info() {
+        if (countPlayers < 6) {
+            freePlace = 6 - countPlayers;
+            System.out.println("Команды неполные. На поле еще есть " + freePlace + " свободных мест");
+        }
+        else {
+            System.out.println("На поле нет свободных мест");
+        }
+    }
+}
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -2,6 +2,12 @@ package day7;
 
 public class Task2 {
     public static void main(String[] args) {
+        Player p = new Player(1);
+        Player p2 = new Player(10);
+        Player p3 = new Player(10);
+        Player p4 = new Player(10);
+
+        p.info();
 
     }
 }
