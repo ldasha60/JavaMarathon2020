09f379c243a58a33283a7cf6de8dc42f443416f5
Dariia Lytvyn <ldasha60@gmail.com>
2021-01-04 23:03:19 +0700

    Add Day8 Solutions

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,28 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String string = "";
+        long before = System.currentTimeMillis();
+
+        for (int i = 0; i <= 20000; i++) {
+            string += i + " ";
+        }
+        long after = System.currentTimeMillis();
+        System.out.println("no SB - " + (after - before));
+        System.out.println(string);
+
+        StringBuilder sb = new StringBuilder();
+
+        long before1 = System.currentTimeMillis();
+
+        for (int i = 0; i <= 20000; i++) {
+            sb.append(i).append(" ");
+        }
+        long after1 = System.currentTimeMillis();
+
+        System.out.println("SB - " + (after1 - before1));
+        System.out.println(sb);
 
     }
+
 }
