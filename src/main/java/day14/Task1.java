89676bbf51efb459d2061fb3f873d77464ad8a8a
ldasha60 <ldasha60@gmail.com>
2021-01-11 16:38:53 +0700

    "Add Day14 Solutions"

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,32 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("input.txt");
+
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+
+            String[] array = line.split(" ");
+
+            if (array.length != 10) {
+                throw new IllegalArgumentException();
+            }
+            int sum = 0;
+            for (String s : array) {
+                sum += Integer.parseInt(s);
+            }
+            System.out.println(sum);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некоректный входной файл");
+        }
 
     }
 }
